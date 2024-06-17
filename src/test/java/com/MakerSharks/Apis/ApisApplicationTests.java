24ac37b3eb2b87234c65327f9574edc63cfb1d4d
Piyush Anand <piyushanand2580@gmail.com>
2024-06-17 17:36:19 +0530

    database configured

---
+++ src/test/java/com/MakerSharks/Apis/ApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MakerSharks.Apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
